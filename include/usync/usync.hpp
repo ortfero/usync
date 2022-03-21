f87b22b14e5f9f9659e8a81870bc8e88b92c6658
ortfero <ortfero@gmail.com>
2022-03-21 17:39:57 +0200

    Reformat with clang-format

--- include/usync/usync.hpp
+++ include/usync/usync.hpp
@@ -6,237 +6,221 @@
 
 
 #include <atomic>
+#include <shared_mutex>
 #include <thread>
 #include <utility>
-#include <shared_mutex>
 
 #if defined(_MSC_VER)
-#include <intrin.h>
+#    include <intrin.h>
 #endif
 
 
 namespace usync {
-    
-    
-static constexpr std::size_t cacheline_size = 64;
-    
-    
-class no_lock {
-public:
-  
-  no_lock() noexcept = default;
-  no_lock(no_lock const&) noexcept = delete;
-  no_lock& operator = (no_lock const&) noexcept = delete;
-  
-  bool try_lock() noexcept { return true; }
-  void unlock() noexcept { }
-  void lock() noexcept { }
-  bool try_lock_shared() noexcept { return true; }
-  void unlock_shared() noexcept { }
-  void lock_shared() noexcept { }
-  
-}; // no_lock
 
 
-class spinlock {
-public:
+    static constexpr std::size_t cacheline_size = 64;
 
-  spinlock() noexcept = default;
-  spinlock(spinlock const&) noexcept = delete;
-  spinlock& operator = (spinlock const&) noexcept = delete;
 
+    class no_lock {
+    public:
+        no_lock() noexcept = default;
+        no_lock(no_lock const&) noexcept = delete;
+        no_lock& operator=(no_lock const&) noexcept = delete;
 
-  bool try_lock() noexcept {
-    if(flag_.load(std::memory_order_relaxed))
-      return false;
+        bool try_lock() noexcept { return true; }
+        void unlock() noexcept {}
+        void lock() noexcept {}
+        bool try_lock_shared() noexcept { return true; }
+        void unlock_shared() noexcept {}
+        void lock_shared() noexcept {}
 
-    return !flag_.exchange(true, std::memory_order_acquire);
-  }
+    };   // no_lock
 
 
-  void unlock() noexcept {
-    flag_.store(false, std::memory_order_release);
-  }
-  
+    class spinlock {
+    public:
+        spinlock() noexcept = default;
+        spinlock(spinlock const&) noexcept = delete;
+        spinlock& operator=(spinlock const&) noexcept = delete;
 
-  void lock() noexcept {
-    while(!try_lock())
-      std::this_thread::yield();
-  }
-  
-  
-  bool try_lock_shared() noexcept {
-      return try_lock();
-  }
-  
-  
-  void unlock_shared() noexcept {
-      unlock();
-  }
-  
-  
-  void lock_shared() noexcept {
-      lock();
-  }
 
+        bool try_lock() noexcept {
+            if(flag_.load(std::memory_order_relaxed))
+                return false;
 
-private:
+            return !flag_.exchange(true, std::memory_order_acquire);
+        }
 
-  alignas(cacheline_size)
-    std::atomic_bool flag_{false};
 
-}; // spinlock
+        void unlock() noexcept {
+            flag_.store(false, std::memory_order_release);
+        }
 
 
-class shared_spinlock {
-public:
+        void lock() noexcept {
+            while(!try_lock())
+                std::this_thread::yield();
+        }
 
-  shared_spinlock() noexcept = default;
-  shared_spinlock(shared_spinlock const&) noexcept = delete;
-  shared_spinlock& operator = (shared_spinlock const&) noexcept = delete;
 
+        bool try_lock_shared() noexcept { return try_lock(); }
 
-  bool try_lock() noexcept {
-    if(data_.writer.load(std::memory_order_relaxed))
-      return false;
 
-    if(data_.writer.exchange(true, std::memory_order_acquire))
-      return false;
+        void unlock_shared() noexcept { unlock(); }
 
-    while(data_.readers.load(std::memory_order_relaxed) > 0)
-      relax();
 
-    return true;
-  }
+        void lock_shared() noexcept { lock(); }
 
 
-  void unlock() noexcept {
-    data_.writer.store(false, std::memory_order_release);
-  }
-  
+    private:
+        alignas(cacheline_size) std::atomic_bool flag_ {false};
 
-  void lock() noexcept {
-    while(!try_lock())
-      std::this_thread::yield();
-  }
+    };   // spinlock
 
 
-  bool try_lock_shared() noexcept {
-    if (data_.writer.load(std::memory_order_relaxed))
-      return false;
+    class shared_spinlock {
+    public:
+        shared_spinlock() noexcept = default;
+        shared_spinlock(shared_spinlock const&) noexcept = delete;
+        shared_spinlock& operator=(shared_spinlock const&) noexcept = delete;
 
-    data_.readers.fetch_add(1, std::memory_order_relaxed);
 
-    bool expected = true;
-    if(data_.writer.compare_exchange_weak(expected, true, std::memory_order_relaxed)) {
-      data_.readers.fetch_sub(1, std::memory_order_relaxed);
-      return false;
-    }
+        bool try_lock() noexcept {
+            if(data_.writer.load(std::memory_order_relaxed))
+                return false;
 
-    return true;
-  }
+            if(data_.writer.exchange(true, std::memory_order_acquire))
+                return false;
 
+            while(data_.readers.load(std::memory_order_relaxed) > 0)
+                relax();
 
-  void unlock_shared() noexcept {
-    data_.readers.fetch_sub(1, std::memory_order_relaxed);
-  }
+            return true;
+        }
 
 
-  void lock_shared() noexcept {
-    while(!try_lock_shared())
-      std::this_thread::yield();
-  }
+        void unlock() noexcept {
+            data_.writer.store(false, std::memory_order_release);
+        }
 
-private:
 
-  alignas(cacheline_size) struct data {
-    std::atomic_bool writer{false};
-    std::atomic_uint readers{0};
-  } data_;
+        void lock() noexcept {
+            while(!try_lock())
+                std::this_thread::yield();
+        }
 
 
-  static void relax() {
-    #if defined(_MSC_VER)
+        bool try_lock_shared() noexcept {
+            if(data_.writer.load(std::memory_order_relaxed))
+                return false;
 
-    #if defined(_M_AMD64) || defined(_M_IX86)
-      _mm_pause();
-    #elif defined(_M_ARM)
-      __yield();
-    #endif // _M_IX86
+            data_.readers.fetch_add(1, std::memory_order_relaxed);
 
-    #else
+            bool expected = true;
+            if(data_.writer.compare_exchange_weak(expected,
+                                                  true,
+                                                  std::memory_order_relaxed)) {
+                data_.readers.fetch_sub(1, std::memory_order_relaxed);
+                return false;
+            }
 
-    #if defined(__x86_64__) || defined(__i386__)
-      __asm__ __volatile__ ("pause");
-    #elif defined(__arm__)
-      __asm__ __volatile__ ("yield");
-    #endif // __i386__
+            return true;
+        }
 
-    #endif // _MSC_VER
-  }
-}; // shared_spinlock
 
+        void unlock_shared() noexcept {
+            data_.readers.fetch_sub(1, std::memory_order_relaxed);
+        }
 
-template<typename T, typename L = spinlock> struct synchronized {
 
-  using resource_type = T;
+        void lock_shared() noexcept {
+            while(!try_lock_shared())
+                std::this_thread::yield();
+        }
 
-  struct unique_access {
+    private:
+        alignas(cacheline_size) struct data {
+            std::atomic_bool writer {false};
+            std::atomic_uint readers {0};
+        } data_;
 
-    unique_access() = delete;
-    unique_access(unique_access const&) = delete;
-    unique_access& operator = (unique_access const&) = delete;
 
-    unique_access(synchronized& owner) noexcept:
-      guard_(owner.lock_), resource_(owner.resource_) { }
+        static void relax() {
+#if defined(_MSC_VER)
 
-    T& operator * () const noexcept { return resource_; }
-    T* operator -> () const noexcept { return &resource_; }
+#    if defined(_M_AMD64) || defined(_M_IX86)
+            _mm_pause();
+#    elif defined(_M_ARM)
+            __yield();
+#    endif   // _M_IX86
 
-  private:
+#else
 
-    std::unique_lock<L> guard_;
-    T& resource_;
+#    if defined(__x86_64__) || defined(__i386__)
+            __asm__ __volatile__("pause");
+#    elif defined(__arm__)
+            __asm__ __volatile__("yield");
+#    endif   // __i386__
 
-  }; // unique_access
+#endif   // _MSC_VER
+        }
+    };   // shared_spinlock
 
 
-  struct shared_access {
+    template<typename T, typename L = spinlock>
+    struct synchronized {
+        using resource_type = T;
 
-    shared_access() = delete;
-    shared_access(shared_access const&) = delete;
-    shared_access& operator = (shared_access const&) = delete;
+        struct unique_access {
+            unique_access() = delete;
+            unique_access(unique_access const&) = delete;
+            unique_access& operator=(unique_access const&) = delete;
 
-    shared_access(synchronized const& owner) noexcept:
-      guard_(owner.lock_), resource_(owner.resource_) { }
+            unique_access(synchronized& owner) noexcept
+                : guard_(owner.lock_), resource_(owner.resource_) {}
 
-    T const& operator * () const noexcept { return resource_; }
-    T const* operator -> () const noexcept { return &resource_; }
+            T& operator*() const noexcept { return resource_; }
+            T* operator->() const noexcept { return &resource_; }
 
-  private:
+        private:
+            std::unique_lock<L> guard_;
+            T& resource_;
 
-    std::shared_lock<L> guard_;
-    T const& resource_;
+        };   // unique_access
 
-  }; // shared_access
 
+        struct shared_access {
+            shared_access() = delete;
+            shared_access(shared_access const&) = delete;
+            shared_access& operator=(shared_access const&) = delete;
 
-  synchronized() = default;
-  synchronized(synchronized const&) = delete;
-  synchronized& operator = (synchronized const&) = delete;
-  synchronized(synchronized&&) = default;
-  synchronized& operator = (synchronized&&) = default;
+            shared_access(synchronized const& owner) noexcept
+                : guard_(owner.lock_), resource_(owner.resource_) {}
 
-  template<typename... Args>
-  synchronized(Args&&... args):
-    resource_(std::forward<Args>(args)...) { }
+            T const& operator*() const noexcept { return resource_; }
+            T const* operator->() const noexcept { return &resource_; }
 
-private:
+        private:
+            std::shared_lock<L> guard_;
+            T const& resource_;
 
-  mutable L lock_;
-  T resource_;
+        };   // shared_access
 
-}; // synchronized
 
+        synchronized() = default;
+        synchronized(synchronized const&) = delete;
+        synchronized& operator=(synchronized const&) = delete;
+        synchronized(synchronized&&) = default;
+        synchronized& operator=(synchronized&&) = default;
 
-} // usync
+        template<typename... Args>
+        synchronized(Args&&... args): resource_(std::forward<Args>(args)...) {}
+
+    private:
+        mutable L lock_;
+        T resource_;
+
+    };   // synchronized
+
+
+}   // namespace usync
--- test/test.cpp
+++ test/test.cpp
@@ -5,7 +5,8 @@
 #include <usync/usync.hpp>
 
 class resource {
-    int value_{0};
+    int value_ {0};
+
 public:
     void turn_up() { ++value_; }
     void turn_down() { --value_; }
@@ -16,26 +17,26 @@ public:
 TEST_CASE("spinlock") {
     resource r;
     usync::spinlock guard;
-    
+
     auto t1 = std::thread([&]() {
         for(int i = 0; i != 1000; ++i) {
             // access to modify
-            std::scoped_lock lock{ guard };
+            std::scoped_lock lock {guard};
             r.turn_up();
         }
     });
-    
+
     auto t2 = std::thread([&]() {
         for(int i = 0; i != 1000; ++i) {
             // access to modify
-            std::scoped_lock lock{ guard };
+            std::scoped_lock lock {guard};
             r.turn_down();
         }
     });
-    
+
     t1.join();
     t2.join();
-    
+
     REQUIRE_EQ(r.value(), 0);
 }
 
@@ -43,58 +44,57 @@ TEST_CASE("spinlock") {
 TEST_CASE("shared_spinlock::shared_spinlock") {
     resource r;
     usync::shared_spinlock guard;
-    
+
     auto t1 = std::thread([&]() {
         for(int i = 0; i != 1000; ++i) {
             // access to modify
-            std::unique_lock lock{ guard };
+            std::unique_lock lock {guard};
             r.turn_up();
         }
     });
-    
+
     auto t2 = std::thread([&]() {
         for(int i = 0; i != 1000; ++i) {
             // access to modify
-            std::unique_lock lock{ guard };
+            std::unique_lock lock {guard};
             r.turn_down();
         }
     });
-    
+
     t1.join();
     t2.join();
-    
-    // access to read
-    std::shared_lock lock{ guard };
-    REQUIRE_EQ(r.value(), 0);
 
+    // access to read
+    std::shared_lock lock {guard};
+    REQUIRE_EQ(r.value(), 0);
 }
 
 
 TEST_CASE("synchronized") {
     using synchronized = usync::synchronized<resource>;
-    
+
     synchronized resource;
-    
+
     auto t1 = std::thread([&]() {
         for(int i = 0; i != 1000; ++i) {
             // access to modify
-            synchronized::unique_access r{ resource };
+            synchronized::unique_access r {resource};
             r->turn_up();
         }
     });
-    
+
     auto t2 = std::thread([&]() {
         for(int i = 0; i != 1000; ++i) {
             // access to modify
-            synchronized::unique_access r{ resource };
+            synchronized::unique_access r {resource};
             r->turn_down();
         }
     });
-    
+
     t1.join();
     t2.join();
-    
+
     // access to read
-    synchronized::shared_access r{ resource };
+    synchronized::shared_access r {resource};
     REQUIRE_EQ(r->value(), 0);
 }
